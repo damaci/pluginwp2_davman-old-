2bca379e1e94e85001cf8c1abb503e36c358e0d9
damaci1718daw2 <damaci1718daw2@gmail.com>
2018-04-25 18:48:14 +0200

    Commit1

--- peupagina.php
+++ peupagina.php
@@ -4,8 +4,8 @@ Plugin Name: Peu de pàgina per defecte per a noves entrades
 Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
-Author: El teu nom i cognom
-Author URI: L'adreça URL del teu compte de Github
+Author: David Manzano Cisneros
+Author URI: http://www.github.com/damaci
 License: GNU GPLv3 
 */ 
 ?>
